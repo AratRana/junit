21de53829038b29bfe239b0b643c17767f0710b3
Arat Rana <arat.rana@jda.com>
2018-02-16 10:14:03 +0530

    jacoco and junit report

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # junit
 Run junit test cases using ant parallel
+
+this include jacoco code coverage example and junit html report
--- junit-example/src/test/java/com/ut/AddUnitTest.java
+++ junit-example/src/test/java/com/ut/AddUnitTest.java
@@ -1,6 +1,7 @@
 package com.ut;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
@@ -12,5 +13,11 @@ public class AddUnitTest {
 		Add add = new Add();
 		int result = add.add(2, 3);
 		assertEquals(5, result);
+		try{
+			int x = 10/0;
+			fail("hello thrown");
+		}catch(Exception e){
+			System.out.println("divided by zero");
+		}
 	}
 }
